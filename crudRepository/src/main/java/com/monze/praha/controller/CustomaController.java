809822f35ad1490e93a43d39066dbe8f7d0bed31
linos nchena <linosnchena@gmail.com>
2020-10-16 01:48:05 +0200

    edit plus create

--- crudRepository/src/main/java/com/monze/praha/controller/CustomaController.java
+++ crudRepository/src/main/java/com/monze/praha/controller/CustomaController.java
@@ -12,55 +12,79 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class CustomaController {
 	@Autowired
 	CustomaService serviceIoC;
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	@RequestMapping(path = "/createdCustomer", method = { RequestMethod.PUT, RequestMethod.POST })
-	public String createCustomer(Customa customaX) 
-	{
-		serviceIoC.creatingCustomer(customaX);
-		return "redirect:/";
-		//return "editContact";
-	}
 
-	@GetMapping(path = { "/added", "/added/{id}" })
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	@GetMapping(path = { "/added", "/added/{id}" }) // with and without id
 	public String readCustomerById(Model customaX, @PathVariable("id") Optional<Long> id)
 			throws RecordNotFoundException {
 		if (id.isPresent()) {
 			Customa entity = serviceIoC.readOneCustomer(id.get());
 			customaX.addAttribute("customer", entity);
 			System.out.print("\n ======================PRESENT===============\n ");
-			System.out.print("\n ====================== "+entity.getId()+" ===============\n ");
+			System.out.print("\n ====================== " + entity.getId() + " ===============\n ");
 			System.out.print("\n ======================PRESENT===============\n ");
+			return "editContact";
 		} else {
 			customaX.addAttribute("customer", new Customa());
 			System.out.print("\n ======================ABSENT===============\n");
-			System.out.print("\n ====================="+id+"===============\n");
+			System.out.print("\n =====================" + id + "===============\n");
 			System.out.print("\n ======================ABSENT===============\n");
+			return "addContact";
 		}
-		return "addContact";
+		//return "addContact";
 	}
 
-	// @GetMapping(path = { "/added", "/added/{id}" })
-	// public String readCustomerById2(Model customaX, @PathVariable("id") Optional<Long> id)
-	// 		throws RecordNotFoundException {
-	// 	if (id.isPresent()) {
-	// 		Customa entity = serviceIoC.readOneCustomer(id.get());
-	// 		customaX.addAttribute("customer", entity);
-	// 	} else {
-	// 		customaX.addAttribute("customer", new Customa());
-	// 	}
-	// 	return "addContact";
-	// }
+	@RequestMapping("/new")
+	public String showNewContactForm(Model model) {
+		Customa Contact = new Customa();
+		model.addAttribute("Customa", Contact);
+		return "create";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveContact(@ModelAttribute("Customa") Customa Contact) {
+		serviceIoC.save(Contact);
+		return "redirect:/";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditContactForm(@PathVariable(name = "id") Long id) {
+		ModelAndView mav = new ModelAndView("edit");
+
+		Customa Customa = serviceIoC.get(id);
+		mav.addObject("Customa", Customa);
+
+		return mav;
+	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	@RequestMapping(path = "/createdCustomer2", method = { RequestMethod.PUT, RequestMethod.POST })
+	public String createCustomer2(Customa customaX) {
+		serviceIoC.creatingCustomer(customaX);
+		return "redirect:/";
+	}
+	@RequestMapping(path = "/createdCustomer", method = { RequestMethod.PUT, RequestMethod.POST })
+	public String createCustomer(Customa customaX) {
+		serviceIoC.creatingCustomer(customaX);
+		return "redirect:/";
+	}
+
+	@RequestMapping(path = "/delete/{id}")
+	public String deleteEmployeeById(Model model, @PathVariable("id") Long id) throws RecordNotFoundException {
+		serviceIoC.deleteEmployeeById(id);
+		return "redirect:/";
+	}
 
 	@RequestMapping
 	public String readCustomers(Model customaX, @Param("keyword") String keyword) {
@@ -69,11 +93,5 @@ public class CustomaController {
 		customaX.addAttribute("keyword", keyword);
 		return "listContacts";
 	}
-
-	@RequestMapping(path = "/delete/{id}")
-	public String deleteEmployeeById(Model model, @PathVariable("id") Long id) throws RecordNotFoundException
-	 {
-		serviceIoC.deleteEmployeeById(id);
-		return "redirect:/";
-	}
+	//////////////////////////////////////////////////////////////////////////////////////////////
 }
--- crudRepository/src/main/java/com/monze/praha/service/CustomaService.java
+++ crudRepository/src/main/java/com/monze/praha/service/CustomaService.java
@@ -1,4 +1,5 @@
 package com.monze.praha.service;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,76 +13,79 @@ import com.monze.praha.repository.CustomaRepository;
 public class CustomaService {
 	@Autowired
 	CustomaRepository repositoryIoC;
-long zed=0;
-
-public Customa readOneCustomer(Long id) throws RecordNotFoundException {
-	System.out.println(" ======== ZED firstr: "+zed+" is identity NUMBER INDENTITY ZED =========\n");
-	Optional<Customa> customer = repositoryIoC.findById(id);
-	if (customer.isPresent()) {
-		System.out.println("\n ================== READONE-CUSTOMER ========================================\n");
-		zed=id;
-		System.out.println(" ======== ZED second: "+zed+" is identity NUMBER INDENTITY ZED =========\n");
-		System.out.println(" Employee number: "+customer.get().getId()+" is IDENTITY =========\n");
-		System.out.println(" Employee number: "+customer.get().getEmail()+" is EMAIL =========\n");
-		System.out.println(" Employee number: "+customer.get().getFirstname()+" is first =========\n");
-		System.out.println(" Employee number: "+customer.get().getLastname()+" is name =========\n");
-		System.out.println(" Employee number: "+customer.get()+" is foundOne =========\n");
-		System.out.println("====================================================================\n");
-		return customer.get();
-	} else {
-		throw new RecordNotFoundException("No such record exist for given id");
+	long zed = 0;
+	public void save(Customa Contact) {
+		repositoryIoC.save(Contact);
 	}
-} // service #4
+	
+	public Customa get(Long id) {
+		return repositoryIoC.findById(id).get();
+	}
+	
+	public Customa readOneCustomer(Long id) throws RecordNotFoundException {
+		System.out.println(" ======== ZED firstr: " + zed + " is identity NUMBER INDENTITY ZED =========\n");
+		Optional<Customa> customer = repositoryIoC.findById(id);
+		if (customer.isPresent()) {
+			System.out.println("\n ================== READONE-CUSTOMER ========================================\n");
+			zed = id;
+			System.out.println(" ======== ZED second: " + zed + " is identity NUMBER INDENTITY ZED =========\n");
+			System.out.println(" Employee number: " + customer.get().getId() + " is IDENTITY =========\n");
+			System.out.println(" Employee number: " + customer.get().getLastname() + " is name =========\n");
+			System.out.println(" Employee number: " + customer.get() + " is foundOne =========\n");
+			System.out.println("====================================================================\n");
+			return customer.get();
+		} else {
+			throw new RecordNotFoundException("No such record exist for given id");
+		}
+	}
 	// service #1 Creating
 
 	public Customa creatingCustomer(Customa customaX) {
-			//	if (customaX.getId() == null) {
-					if (customaX.getLastname() != null) {
+		if (customaX.getId() == null) {
+			// if (customaX.getLastname() != null) {
 			System.out.println("\n ================== CREATE ONE ========================================\n");
-			System.out.println(" Employee number: "+customaX.getLastname()+" is CREATED 1=========\n");
-			System.out.println(" Employee number: "+customaX.getFirstname()+" is CREATED 1=========\n");
-			System.out.println(" Employee number: "+customaX.getId()+" is CREATED 1=========\n");
+			System.out.println(" Employee number: " + customaX.getLastname() + " is CREATED 1=========\n");
+			System.out.println(" Employee number: " + customaX.getId() + " is CREATED 1=========\n");
 			System.out.println("====================================================================\n");
 			customaX = repositoryIoC.save(customaX);
 			return customaX;
 		} else {
-		
-				System.out.println("\n ================== EDIT ONE ========================================\n");
-				System.out.println(" Employee number: "+customaX.getLastname()+" is EDITED 2=========\n");
-				System.out.println(" Employee number: "+customaX.getFirstname()+" is EDITED 2=========\n");
-				System.out.println(" Employee number: "+customaX.getId()+" is EDITED 2=========\n");
-				System.out.println("======================END ==============================================\n");
-			//	Customa customer = customaX;// repositoryIoC.findById(customaX.getId());
-				//Optional<Customa> customer = repositoryIoC.findById(customaX.getId());
-			//	if (customer.isPresent()) {
-				Customa newEntity=customaX;// =null;//= customer.get();
+
+			System.out.println("\n ================== UPDATE ONE ========================================\n");
+			System.out.println(" Employee number: " + customaX.getLastname() + " is EDITED 2=========\n");
+			System.out.println(" Employee number: " + customaX.getId() + " is EDITED 2=========\n");
+			System.out.println("======================END ==============================================\n");
+			Optional<Customa> customerAvailable = repositoryIoC.findById(customaX.getId());
+			if (customerAvailable.isPresent()) {
+				Customa newEntity = customerAvailable.get();
+
 				newEntity.setFirstname(customaX.getFirstname());
 				newEntity.setLastname(customaX.getLastname());
 				newEntity.setEmail(customaX.getEmail());
+				newEntity.setId(customaX.getId());
 				newEntity = repositoryIoC.save(newEntity);
 				return newEntity;
-		//	} 
-			// else {			
-			// 	System.out.println("\n ================== EDIT TWO ========================================\n");
-			// 	System.out.println(" Employee number: "+customaX.getLastname()+" is THIRD =========\n");
-			// 	System.out.println(" Employee number: "+customaX.getFirstname()+" is THIRD =========\n");
-			// 	System.out.println(" Employee number: "+customaX.getId()+" is THIRD =========\n");
-			// 	System.out.println("=======================END=========================================\n");
-			// 	customaX = repositoryIoC.save(customaX);
-			// 	return customaX;
-			// }
+			} else {
+				System.out.println("\n ================== UPDATE-EDIT ========================================\n");
+				System.out.println(" Employee number: " + customaX.getLastname() + " is THIRD =========\n");
+				System.out.println(" Employee number: " + customaX.getFirstname() + " is THIRD =========\n");
+				System.out.println(" Employee number: " + customaX.getId() + " is THIRD =========\n");
+				System.out.println("=======================END=========================================\n");
+				customaX = repositoryIoC.save(customaX);
+				return customaX;
+			}
 		}
 	} // service #2 & sevice #3
 
 	public List<Customa> readCustomers(String keyword) {
-		List<Customa> result;		
-		if(keyword!=null)
-		{
-		result= (List<Customa>) repositoryIoC.findByLastname(keyword);
+		List<Customa> result;
+		if (keyword != null) {
+			result = (List<Customa>) repositoryIoC.findByLastname(keyword);
+		} else {
+			result = (List<Customa>) repositoryIoC.findAll();
 		}
-		else{result= (List<Customa>) repositoryIoC.findAll();}
 		System.out.println("\n ================== LOADING MANY ========================================\n");
-		System.out.println(" Employee number: "+result+" is many =========\n");
+		System.out.println(" Employee number: " + result + " is many =========\n");
 		System.out.println("====================================================================\n");
 		if (result.size() > 0) {
 			return result;
@@ -92,15 +96,15 @@ public Customa readOneCustomer(Long id) throws RecordNotFoundException {
 
 	public void deleteEmployeeById(Long id) throws RecordNotFoundException {
 		Optional<Customa> participant = repositoryIoC.findById(id);
-			if (participant.isPresent()) {
+		if (participant.isPresent()) {
 
 			System.out.println("\n ==================DELETE ===========================================\n");
-			long zed=id;
-			System.out.println(" Employee number: "+zed+" is deleted =========\n");
+			long zed = id;
+			System.out.println(" Employee number: " + zed + " is deleted =========\n");
 			System.out.println("====================================================================\n");
 			repositoryIoC.deleteById(id);
 		} else {
 			throw new RecordNotFoundException("No such record exist in the database");
 		}
-	} 
+	}
 }
