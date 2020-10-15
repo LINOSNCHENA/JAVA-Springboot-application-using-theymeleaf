3be20d0e28ccfe153eb99dac1279813562e9fa1e
LINOS NCHENA <linosnchena@gmail.com>
2020-10-15 11:07:41 +0200

    updated query for filtering

--- crudRepository/src/main/java/com/monze/praha/repository/CustomerRepository.java
+++ crudRepository/src/main/java/com/monze/praha/repository/CustomerRepository.java
@@ -10,7 +10,7 @@ import com.monze.praha.model.CustomerEntity;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
-	@Query("SELECT contactX FROM CustomerEntity contactX WHERE CONCAT(contactX.first_name, ' ', contactX.last_name, ' ', contactX.email) LIKE %?1%")
+	@Query("SELECT X FROM CustomerEntity X WHERE CONCAT(X.firstName, ' ', X.lastName, ' ', X.email) LIKE %?1%")
 	 List<CustomerEntity> findByLastname(String lastname);
 	// User findByEmailAddress(String emailAddress);
 }
